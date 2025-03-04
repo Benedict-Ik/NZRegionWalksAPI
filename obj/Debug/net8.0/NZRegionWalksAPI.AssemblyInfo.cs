a5b16489c7622233b5df936a5293874fad3252ef
Benedict Odoh <benedict.ik98@gmail.com>
2025-03-04 19:12:41 +0100

    latest commit

--- obj/Debug/net8.0/NZRegionWalksAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/NZRegionWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb2c36f240995a523f581988c6c4874124a9b8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea32a7a119492cf59811be710c00d106c7d0481a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
