ab834c6837c4d24590a667af487cee7ca87fefe7
Benedict Odoh <benedict.ik98@gmail.com>
2025-03-04 20:07:47 +0100

    latest commit

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZRegionWalksAPI.Data;
+
+namespace NZRegionWalksAPI.Controllers
+{
+    // {baseUrl}/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZRegionWalksDbContext _dbContext;
+
+        public RegionsController(NZRegionWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        // GET: {baseUrl}/api/regions
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            /*Hardcoded*/
+            /*var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/auckland/auckland-region.jpg"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/wellington/wellington-region.jpg"
+                }
+            };*/
+
+            var regions = _dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+    }
+}
--- obj/Debug/net8.0/NZRegionWalksAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/NZRegionWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea32a7a119492cf59811be710c00d106c7d0481a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b16489c7622233b5df936a5293874fad3252ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
