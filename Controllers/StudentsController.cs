e9911bfa8da210ef6b6843eccc4c9cadf057692f
Benedict Odoh <benedict.ik98@gmail.com>
2025-03-04 16:50:01 +0100

    latest commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZRegionWalksAPI.Controllers
+{
+    // {baseUrl}/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // Get: {baseUrl}/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] { "John", "Doe", "Jane", "Doe" };
+            return Ok(students);
+        }
+    }
+}
