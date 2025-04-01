26af52894e42c9cb1550be5c4498953ce7b1fb13
Benedict Odoh <benedict.ik98@gmail.com>
2025-04-01 12:25:11 +0100

    Added image

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Here is what we did in this branch:
 - Ensure the `Solution Configuration` dropdown is on `Debug`.
 - The above steps will ensure both projects are opened in different browsers (web pages) when you run the app.
 
-![File](File.png)
+![File](file.png)
 
 - Now, since we arealy have git tracking the `API` project, we can as well create a remote repository to do same for the `MVC` project by following the steps below:
     - Create a new repository on Github called `NZRegionWalksMVC`.
--- obj/Debug/net8.0/NZRegionWalksAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/NZRegionWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495fff3b044cb54f87bdeb2b2c1b56e4b2b69853")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db882df61232533c4b92931c1502015e840377e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZRegionWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
